a24f9a555506c8c8dc6760c101171607ab179673
KwintenP <kwinten.pisman@gmail.com>
2017-12-13 20:07:12 +0100

    Added empty angular lci project to test.

--- schematics-test/src/app/app.module.ts
+++ schematics-test/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HotelModule } from './test/hotel.module';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HotelModule
   ],
   providers: [],
   bootstrap: [AppComponent]
